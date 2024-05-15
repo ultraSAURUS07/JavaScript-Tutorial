f302a7882575f7cb9e6a0df71e629ac718aeb12c
ultra saurus <rohithakur184@gmail.com>
2024-05-16 00:45:39 +0530

    first commit

---
+++ 01_Basics/01_datatypes.js
@@ -0,0 +1,29 @@
+"use strict";   //treat all JS code as newer version in this JS file.
+
+// alert(3+3);     //We are using nodejs not browser so this won't work here.
+
+let name = "hitesh";
+let age = 45;
+let isLoggedIn = true;
+
+//Primitive datatypes
+
+//number => 2 to power 53;
+//bigint
+//string => ""
+//boolean => true/false
+//null => It is a standaloone value
+//undefined => 
+
+//Object
+
+
+console.log("=================================================");
+let a = null;
+let b ;
+console.table([a, b]);
+
+console.log("=================================================");
+console.log(typeof(null));
+console.log(typeof(undefined));
+
---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,21 @@
+const accountId = 144512;
+let accountEmail = "hitesh@google.com";
+var accountPassword = "12345";  //Do not use it
+accountCity = "Jaipur"; // Not recommended.
+
+// accountId = 2;   //Not allowed
+// console.log(accountId);
+
+accountEmail = "hc@hc.com";
+accountPassword = "1254";
+accountCity = "Bengaluru";  //Do not use it
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
+//Use let and const only
+
+let accountState ;
+console.log(accountState);  //This will print undefined.
+
+
+
